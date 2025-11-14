49f43152d293cc394bf8db3f0f01f0cd168bda29
chasewyngarden <chwy3948@colorado.edu>
2025-11-14 15:35:20 -0700

    Update Part2Task1.m

--- Part2Task1.m
+++ Part2Task1.m
@@ -14,21 +14,22 @@ geo_t =
 geo_r =
 
 
-  
 function [e,c_L,c_Di] = PLLT(b,a0_t,a0_r,c_t,c_r,aero_t,aero_r,geo_t,geo_r,N)
 
 % solving for A1 and An 
 
-for i = 1:10
+for i = 1:length(N)
 
-    theta0(i) = (i*pi)/(2*N); 
-    ctheta_0(i) = -(acos(theta0)*(c_t - c_r)) + c_r
-    atheta_0 = 
+    theta_i(i) = (i*pi)/(2*N); 
+    ctheta_i(i) = -(cos(thetai(i)*(c_t - c_r)) + c_r
+    aL0_theta_i = 2*pi
+    A0A = linspace(-5,35,100)
+
     for j = 1:N
 
-  
-    
-        Mij = (4*b)/
+        Mij = ((4*b)/aL0_theta_i)*sin((2*j - 1)*theta_i) + (2*j - 1)*(sin((2*j - 1)*theta_i)/sin(theta_i)
+
+        di = AOA - AL0
 
     end 
 
