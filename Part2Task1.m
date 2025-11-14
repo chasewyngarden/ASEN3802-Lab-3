457b74b7228aa07fefc3e8d04f457c2394620e43
chasewyngarden <chwy3948@colorado.edu>
2025-11-14 14:47:57 -0700

    Update Part2Task1.m

--- Part2Task1.m
+++ Part2Task1.m
@@ -2,4 +2,26 @@ clear;
 clc;
 close all;
 
-fvfvf
+N = 10;
+b = 
+a0_t = 
+a0_r = 
+
+function [e,c_L,c_Di] = PLLT(b,a0_t,a0_r,c_t,c_r,aero_t,aero_r,geo_t,geo_r,N)
+
+
+% solving for A1 and An 
+
+
+
+for i = 1:10
+
+    theta0(i) = (i*pi)/(2*N); 
+
+    for j = 1:N
+
+        Mij = (4*b)/
+
+    end 
+
+end 
