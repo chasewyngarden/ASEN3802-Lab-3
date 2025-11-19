e20d251bb77dbdf24207dfaee9be57f53a2bc5c1
Andrei-Teo <ante8876@colorado.edu>
2025-11-18 20:48:55 -0700

    Add files via upload

---
+++ Part2Task2.m
@@ -0,0 +1,122 @@
+
+clear;
+clc;
+close all;
+
+
+% Define constants
+N = 50;
+% b = 100;
+a0_t = 2*pi;
+a0_r = 2*pi;
+% c_t = 10;
+c_r = 10;
+aero_t = 0;
+aero_r = 0;
+geo_t = (5*pi)/180;
+geo_r = (5*pi)/180;
+
+
+
+lambda_vec = linspace(0, 1, 100); % taper ration c_t/c_r
+AR_vec = [4, 6, 8, 10]; % aspect ratios from figure 5.20
+delta_mat  = zeros(length(AR_vec), length(lambda_vec));
+
+for i = 1:length(AR_vec)
+    AR = AR_vec(i);
+
+    for j = 1:length(lambda_vec)
+        lambda = lambda_vec(j);
+        c_t = lambda * c_r;  % tip chord from taper ratio
+        c_avg = (c_t + c_r) / 2; % average chord length to find S
+
+        b = AR * c_avg; % AR = b/c_avg
+    
+        %Call funtion
+        [e,c_L,c_Di] = PLLT(b,a0_t,a0_r,c_t,c_r,aero_t,aero_r,geo_t,geo_r,N);
+    
+        % induced drag factor delta from e (delta = (1/e) - 1
+        delta_mat(i, j) = 1./e - 1; % delta matrix
+    end
+end
+
+
+figure;
+hold on
+for i = 1:length(AR_vec)
+    plot(lambda_vec, delta_mat(i,:), 'LineWidth', 1.5);
+end
+xlabel('Taper Ratio, c_t / c_r');
+ylabel('\delta');
+title('Induced Drag Factor \delta vs. Taper Ratio c_t / c_r');
+legend('AR = 4','AR = 6','AR = 8','AR = 10','Location','best');
+hold off
+
+
+
+
+
+function [e,c_L,c_Di] = PLLT(b,a0_t,a0_r,c_t,c_r,aero_t,aero_r,geo_t,geo_r,N)
+
+% solving for A1 and An 
+
+    m = 1:N;
+
+    theta_i = (m*pi)/(2*N); %define theta 
+
+
+    % Variables depending on span
+    ctheta_i = c_r -(cos(theta_i)*(c_r - c_t));
+
+    a0_theta_i  = a0_r - (cos(theta_i)*(a0_r - a0_t));
+
+    alphaL0_theta_i = aero_r - (cos(theta_i)*(aero_r - aero_t));
+
+    AOA_theta_i = geo_r - (cos(theta_i)*(geo_r - geo_t));
+
+    % Effective alpha
+
+    di = (AOA_theta_i - alphaL0_theta_i)';
+
+    %
+    
+    for i = 1:N
+
+      for j = 1:N
+
+        Mij(i,j) = ((4*b)/(a0_theta_i(i)*ctheta_i(i)))*sin((2*j - 1)*theta_i(i)) + (2*j - 1)*((sin((2*j - 1)*theta_i(i)))/sin(theta_i(i)));
+
+      end  
+       
+    end 
+
+% solve matrix using \
+A = Mij\di;
+
+
+% find AR and caluclate CL
+cave = (c_t + c_r)/2;
+AR = b/cave;
+
+c_L = A(1)*pi*AR;
+
+% delta calculation
+    del = 0;
+for k = 2:N
+
+    del = del + ((2*k -1) * (A(k)/A(1))^2);
+
+end 
+
+    %CD and E calulation based on delta
+    c_Di = ((c_L)^2) / (pi*AR) * (1 + del);
+
+    e = 1/(1+del);
+end
+
+
+
+
+
+
+
